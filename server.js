0ff131c2ab188d7f9dbc0523bb8a069ec6558e15
Jaydeep Patel <jaydeep.patel@innvonix.com>
2026-05-05 18:27:40 +0530

    updt: remove log

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
-console.log(MONGO_URI)
 // Connect to MongoDB
 mongoose.connect(MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
